1203aa389048a327bb1f9f201166518c025f342f
Vitallios <61820576+vitallios@users.noreply.github.com>
2024-02-29 16:10:38 +0300

    reset fail

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/vHeader.vue'
 </script>
 
 <template>
-  <header>
-
-    <div class="wrapper">
-      <div class="container">
-          <nav class="nav__global">
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
-      </div>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <Header />
+  </div>
 </template>
-
-
-<style>
-.nav__global {
-  display: flex;
-  gap: 1em;
-  padding: 2vh 0;
-}
-
-.container {
-  width: 80%;
-  margin: 0 auto;
-}
-
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-
-a {
-  text-decoration: none;
-  font-size: 2vh;
-  color: #090909;
-  font-weight: 500;
-}
-</style>
---
+++ src/components/vHeader.vue
@@ -0,0 +1,46 @@
+<template>
+ <header>
+
+  <div class="wrapper">
+   <div class="container">
+    <nav class="nav__global">
+     <RouterLink to="/">Home</RouterLink>
+     <RouterLink to="/about">About</RouterLink>
+    </nav>
+   </div>
+  </div>
+ </header>
+
+ <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<style>
+.nav__global {
+ display: flex;
+ gap: 1em;
+ padding: 2vh 0;
+}
+
+.container {
+ width: 80%;
+ margin: 0 auto;
+}
+
+* {
+ padding: 0;
+ margin: 0;
+ box-sizing: border-box;
+}
+
+a {
+ text-decoration: none;
+ font-size: 2vh;
+ color: #090909;
+ font-weight: 500;
+}
+</style>
